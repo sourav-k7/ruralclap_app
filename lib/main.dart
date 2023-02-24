afbc119965fae26b88d733842d65c7304a1bf37f
utkarshk8 <utkr8402@gmail.com>
2023-02-24 13:19:50 +0530

    added font roboto to theme

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Ruralclap',
       theme: ThemeData(
-        fontFamily: 'Open Sans',
+        fontFamily: 'Roboto',
         primarySwatch: const MaterialColor(0xFF0066ff, <int, Color>{
           50: Color(0xFF5C9DFF),
           100: Color(0xFF4791FF),
